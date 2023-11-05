9311f1f3cdd1a31ac89f087bcab7617faf4b43d5
Pasang-Tamang <pasang.tamang9824@gmail.com>
2023-11-05 13:51:46 +0545

    feat:integrated signup page

---
+++ frontend/src/pages/auth/SignUp/index.tsx
@@ -0,0 +1,119 @@
+import Container from "@mui/material/Container";
+import { Col, Row, Form } from "react-bootstrap";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import {
+  errorToast,
+  successToast,
+  warningToast,
+} from "../../../services/toaster.service";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const registerHandler = async (e: any) => {
+    e.preventDefault();
+    console.log("clicked");
+    console.log(name, email, password, confirmPassword);
+
+    if (password !== confirmPassword) {
+      warningToast("Password and confirm Password must be same");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+        confirmPassword,
+      };
+
+      try {
+        const response = await axios.post(
+          "http://localhost:8080/api/v1/auth/register",
+          userData
+        );
+        console.log(response);
+
+        if (response.data.status) {
+          navigate("/");
+          successToast(response.data.message);
+        }
+      } catch (error: any) {
+        console.log(error);
+
+        errorToast(error.response.data.error);
+      }
+    }
+  };
+  return (
+    <Container>
+      <Row className="d-flex justify-content-center">
+        <Col xs={12} md={6}>
+          <h1>SignUp</h1>
+
+          <Form onSubmit={registerHandler}>
+            <TextField
+              id="name"
+              label="Name"
+              onChange={(e) => setName(e.target.value)}
+              variant="outlined"
+              required
+              placeholder="Enter your name"
+              fullWidth
+              autoFocus
+              className="mb-4"
+            />
+
+            <TextField
+              id="email"
+              label="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              variant="outlined"
+              required
+              placeholder="Enter your Email"
+              fullWidth
+              autoFocus
+              className="mb-4"
+            ></TextField>
+
+            <TextField
+              id="password"
+              label="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+              required
+              fullWidth
+              variant="outlined"
+              autoFocus
+              className="mb-4"
+            ></TextField>
+
+            <TextField
+              id="confirm-password"
+              label="Confirm Password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              variant="outlined"
+              required
+              fullWidth
+              autoFocus
+              placeholder="Confirm the Password"
+              className="mb-4"
+            ></TextField>
+
+            <Button type="submit" variant="contained">
+              SignUp
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default SignUp;
